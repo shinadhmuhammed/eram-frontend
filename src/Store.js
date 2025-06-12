3ced239592968f80f86be90f0a6cf0f9a8f045c3
shinadhmuhammed <muhammedshinad2@gmail.com>
2025-06-12 12:02:18 +0530

    env changes

--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userAuthReducer from "./Slices/Users/UserSlice.jsx";
-import superAdminAuthReducer from "./Slices/SuperAdmin/SuperAdminSlice.jsx";
-import { userApi } from "./Slices/Users/UserApis.jsx";
-import { superAdminApi } from "./Slices/SuperAdmin/SuperAdminAPIs.jsx";
-import { adminApi } from "./Slices/Admin/AdminApis.jsx";
+import userAuthReducer from "./Slices/Users/UserSlice.js";
+import superAdminAuthReducer from "./Slices/SuperAdmin/SuperAdminSlice.js";
+import { userApi } from "./Slices/Users/UserApis.js";
+import { superAdminApi } from "./Slices/SuperAdmin/SuperAdminAPIs.js";
+import { adminApi } from "./Slices/Admin/AdminApis.js";
 
 const store = configureStore({
   reducer: {
